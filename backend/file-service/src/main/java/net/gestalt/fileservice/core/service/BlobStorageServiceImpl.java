469766177bb0b588ae53393f48387ea885a71639
OliverKeefe <oliverkeefe@tutanota.com>
2025-10-10 09:44:08 +0100

    feat(backend): add BlobStorageServiceImpl.java

---
+++ backend/file-service/src/main/java/net/gestalt/fileservice/core/service/BlobStorageServiceImpl.java
@@ -0,0 +1,6 @@
+package net.gestalt.fileservice.core.service;
+
+import net.gestalt.fileservice.core.port.inbound.StorageServicePort;
+
+public class BlobStorageServiceImpl implements StorageServicePort {
+}
