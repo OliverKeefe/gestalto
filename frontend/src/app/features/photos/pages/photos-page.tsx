c50272f3a14db965adce87681e86267a67656617
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-27 09:47:59 +0000

    feat: update Photos page

--- frontend/src/app/features/photos/pages/photos-page.tsx
+++ frontend/src/app/features/photos/pages/photos-page.tsx
@@ -1,12 +1,10 @@
 import {PhotoCard} from "@/app/features/shared/components/cards/photo-card.tsx";
+import {MasonryGridGallery} from "@/app/features/shared/components/galleries/masonry-gallery.tsx";
 
 export default function Photos() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <p>Photos Page Route Success</p>
-                <PhotoCard />
-            </div>
+            <MasonryGridGallery />
         </div>
     )
 }
