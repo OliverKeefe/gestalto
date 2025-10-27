9b40d7f7468065a13eaa6b8b3d3cd6fede2597e4
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-10-27 09:47:42 +0000

    feat: add placeholder to PhotoCard component

--- frontend/src/app/features/shared/components/cards/photo-card.tsx
+++ frontend/src/app/features/shared/components/cards/photo-card.tsx
@@ -2,11 +2,13 @@ import {Card, CardContent} from "@/components/ui/card";
 import { React } from 'react';
 
 export function PhotoCard() {
+    const placeholder: string = "https://placehold.co/600x400/000000/FFFFFF/png";
 
     return (
         <Card>
             <CardContent>
-                <img src={"https://placehold.co/600x400/000000/FFFFFF/png"} />
+                <img src={"https://placehold.co/600x400/000000/FFFFFF/png" || placeholder} />
+                <p>Photo title</p>
             </CardContent>
         </Card>
     );
