77d131428df53f96766ca0949dd77701e24ada2e
OliverKeefe <oliverkeefe@tutanota.com>
2025-10-31 17:44:19 +0000

    feat(backend): add http server go routine

--- backend/cmd/gestalt/main.go
+++ backend/cmd/gestalt/main.go
@@ -1,1 +1,29 @@
-package gestalt
+package main
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	port := ":8081"
+
+	go func() {
+		fmt.Printf("starting backend on port %s\n", port)
+
+		err := http.ListenAndServe(port, mux)
+		if err != nil {
+			fmt.Errorf("error starting backend", err)
+			os.Exit(1)
+		}
+	}()
+
+	//p := tea.NewProgram(initialModel())
+	//if _, err := p.Run(); err != nil {
+	//	fmt.Errorf("tui error", err)
+	//}
+}
