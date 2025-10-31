cec10b480bad7bdb8ced0b52a9a2c421b830c14d
OliverKeefe <oliverkeefe@tutanota.com>
2025-10-31 17:40:36 +0000

    feat(backend): implement File struct

---
+++ backend/core/files/model/file_model.go
@@ -0,0 +1,14 @@
+package model
+
+type File struct {
+	Metadata MetaData
+	FileData []byte
+}
+
+type MetaData struct {
+	Filename     string
+	Size         uint64
+	Permissions  fs.FileMode
+	LastModified time.Time
+	IsDirectory  bool
+}
--- backend/core/files/model/models.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type File struct {
-}
-
-type MetaData struct {
-}
