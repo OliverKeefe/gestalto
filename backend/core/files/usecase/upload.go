197d86841e15c2c5f88f5af2e9de06e6f3464b22
OliverKeefe <oliverkeefe@tutanota.com>
2025-10-31 19:17:35 +0000

    feat(backend): add RegisterRoutes method

--- backend/core/files/usecase/upload.go
+++ backend/core/files/usecase/upload.go
@@ -2,8 +2,13 @@ package files
 
 import (
 	model "backend/core/files/model"
+	"net/http"
 )
 
 type UploadFile struct {
 	file model.File
+}
+
+func (upload UploadFile) RegisterRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/files/upload", upload.Api)
 }
