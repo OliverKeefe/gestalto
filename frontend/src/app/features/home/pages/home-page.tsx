f9a01d4c6557ff56e0033561a32bc27319357823
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-03 21:44:21 +0000

    feat(frontend): add UsageCard component to Home page

--- frontend/src/app/features/home/pages/home-page.tsx
+++ frontend/src/app/features/home/pages/home-page.tsx
@@ -9,6 +9,7 @@ export function Home() {
     return (
         <div className="flex flex-1 flex-col">
                     <Container>
+                        <UsageCard />
                         <h1 className="text-2xl font-semibold p-6 m-1">All files</h1>
                         <FileTable />
                     </Container>
