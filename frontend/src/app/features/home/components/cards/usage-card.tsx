36337bc09a19df154e0d112c5db20594947f44a9
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-03 21:43:41 +0000

    feat(frontend): add UsageCard component

---
+++ frontend/src/app/features/home/components/cards/usage-card.tsx
@@ -0,0 +1,23 @@
+
+export function UsageCard() {
+    const usedStorage = 200;
+    const totalFiles = 354;
+    const totalDocuments = 243;
+    return (
+        <Card>
+            <CardContent className={"w-full flex flex-col"}>
+                <CardTitle>Status</CardTitle>
+                <CardDescription>Usage information and service health.</CardDescription>
+                <span className={"pt-5 w-full"}>
+                    <ul className={"flex w-full items-center"}>
+                        <li className={"flex-1 text-center"} >Storage Used: {usedStorage} GB</li>
+                        <li className={"flex-1 text-center"}>Files: {totalFiles}</li>
+                        <li className={"flex-1 text-center"}>Documents: {totalDocuments}</li>
+                        <li className={"flex-1 text-center"}>Documents: {totalDocuments}</li>
+                    </ul>
+                </span>
+            </CardContent>
+            <CardFooter>Yo mama</CardFooter>
+        </Card>
+    );
+}
