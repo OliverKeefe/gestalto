4b0c4d20ed3d3d04881be8a77ac339bebc988cb6
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-04 23:50:58 +0000

    feat(frontend): add settings route

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -3,6 +3,7 @@ import Login from "@/app/features/auth/pages/login-page";
 import { Home } from "@/app/features/home/pages/home-page";
 import Documents from "@/app/features/documents/pages/documents-page.tsx";
 import Photos from "@/app/features/photos/pages/photos-page";
+import Settings from "@/app/features/settings/pages/settings-page";
 
 const AppRoutes = () => {
     return (
@@ -12,6 +13,7 @@ const AppRoutes = () => {
             <Route path="/home" element={<Home />} />
             <Route path="/documents" element={<Documents />} />
             <Route path="/photos" element={<Photos />} />
+            <Route path="/settings" element={<Settings />} />
         </Routes>
     );
 };
