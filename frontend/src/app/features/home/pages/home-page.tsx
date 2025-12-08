7f5000c9e048ea5d6d8e63618f6a2e814dff1f1d
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-08 06:21:50 +0000

    refactor(frontend): remove header

--- frontend/src/app/features/home/pages/home-page.tsx
+++ frontend/src/app/features/home/pages/home-page.tsx
@@ -6,7 +6,6 @@ export function Home() {
     return (
         <div className="flex flex-1 flex-col">
                     <Container>
-                        <h1 className="text-2xl font-semibold p-6 m-1">All files </h1>
                         <FileTable />
                     </Container>
             </div>
