d1967f2c7b466164c0d183ee7f249479569551ca
Keefe, Oliver B <ok22786@essex.ac.uk>
2025-12-09 13:44:30 +0000

    feat(frontend): add settings-page.tsx

---
+++ frontend/src/app/features/settings/pages/settings-page.tsx
@@ -0,0 +1,11 @@
+import {UsageCard} from "@/app/features/home/components/cards/usage-card.tsx";
+import {Container} from "@/app/features/shared/components/layout/container.tsx";
+
+
+export default function Settings() {
+    return (
+        <Container>
+            <UsageCard />
+        </Container>
+    );
+}
