d8e617dae1511779de14279195949d5e7eb22908
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-13 17:04:52 +0000

    feat(backend): add HTTPClient config

--- backend/src/internal/bootstrap/bootstrap.go
+++ backend/src/internal/bootstrap/bootstrap.go
@@ -1,7 +1,13 @@
 package bootstrap
 
+import (
+	"net/http"
+	"time"
+)
+
 type Config struct {
-	BaseURL string
+	BaseURL    string
+	HTTPClient *http.Client
 }
 
 type Option func(*Config)
@@ -9,6 +15,9 @@ type Option func(*Config)
 func NewConfig(opts ...Option) Config {
 	cfg := Config{
 		BaseURL: "",
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
 	}
 	for _, opt := range opts {
 		opt(&cfg)
@@ -19,5 +28,11 @@ func NewConfig(opts ...Option) Config {
 func WithBaseURL(baseUrl string) Option {
 	return func(c *Config) {
 		c.BaseURL = baseUrl
+	}
+}
+
+func WithHTTPClient(client *http.Client) Option {
+	return func(c *Config) {
+		c.HTTPClient = client
 	}
 }
