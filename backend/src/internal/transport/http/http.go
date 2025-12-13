ef8f0dbdf60d01d8c423ca90e52f22450fd4685b
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-13 17:04:26 +0000

    feat(backend): implement Post method

--- backend/src/internal/transport/http/http.go
+++ backend/src/internal/transport/http/http.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"backend/src/internal/bootstrap"
+	"bytes"
+	"encoding/json"
 	"net/http"
 )
 
@@ -9,8 +11,23 @@ type AppConfig struct {
 	ApiConfig *bootstrap.Config
 }
 
-func (a AppConfig) Post() (http.Response, error) {
-	panic("Not implemented.")
+func (a AppConfig) Post(endpointURL string, payload any) (*http.Response, error) {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	url := a.buildURL(endpointURL)
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	return client.Do(req)
 }
 
 func (a AppConfig) Get() (http.Response, error) {
