1e527fb998e23d2192040574ce37e2180da41fda
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-14 16:27:46 +0000

    refactor(backend): improve names

--- backend/src/internal/transport/http/http.go
+++ backend/src/internal/transport/http/http.go
@@ -8,16 +8,16 @@ import (
 )
 
 type AppConfig struct {
-	ApiConfig *bootstrap.Config
+	Config *bootstrap.Config
 }
 
-func (a AppConfig) Post(endpointURL string, payload any) (*http.Response, error) {
+func (app AppConfig) Post(endpointURL string, payload any) (*http.Response, error) {
 	body, err := json.Marshal(payload)
 	if err != nil {
 		return nil, err
 	}
 
-	url := a.buildURL(endpointURL)
+	url := app.buildURL(endpointURL)
 
 	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
 	if err != nil {
@@ -25,27 +25,31 @@ func (a AppConfig) Post(endpointURL string, payload any) (*http.Response, error)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
+	client := app.Config.HTTPClient
 
-	client := &http.Client{}
 	return client.Do(req)
 }
 
-func (a AppConfig) Get() (http.Response, error) {
+func (app AppConfig) Get() (http.Response, error) {
 	panic("Not implemented.")
 }
 
-func (a AppConfig) Delete() (http.Response, error) {
+func (app AppConfig) Delete() (http.Response, error) {
 	panic("Not implemented.")
 }
 
-func (a AppConfig) Put() (http.Response, error) {
+func (app AppConfig) Put() (http.Response, error) {
 	panic("Not implemented.")
 }
 
-func (a AppConfig) Trace() (http.Response, error) {
+func (app AppConfig) Trace() (http.Response, error) {
 	panic("Not implemented.")
 }
 
-func (a AppConfig) Patch() (http.Response, error) {
+func (app AppConfig) Patch() (http.Response, error) {
 	panic("Not implemented.")
+}
+
+func (app AppConfig) buildURL(endpointUrl string) string {
+	return app.Config.BaseURL + endpointUrl
 }
