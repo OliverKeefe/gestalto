8c240d49e11a4c42b3acdb43be54e437631ee205
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2025-12-29 19:00:03 +0000

    feat(frontend): add upload.ts
    * add PayloadItem type
    
    * add UploadForm class
    
    * add prepare method, async method that calls extractMetadata and assigns file binary data, file metadata and the metadata's id field as key to a map of PayloadItem's.
    
    * add buildFormData method, takes the aforementioned payload map and appends the metadata and the file data to a FormData object.
    
    * add send method, sends FormData to upload api endpoint via HTTP POST.
    
    * add handleFailedUpload helper method, response message if required.

---
+++ frontend/src/app/features/shared/files/upload.ts
@@ -0,0 +1,66 @@
+import {extractMetadata, type Metadata} from "@/app/features/shared/files/metadata";
+
+type PayloadItem = {
+    id: string
+    metadata: Metadata
+    file: File
+}
+
+// type Payload = Record<string, PayloadItem>
+
+export class UploadForm {
+    private readonly files: File[];
+    private payload: PayloadItem[];
+    private readonly formData: FormData;
+
+    constructor(files: File[]) {
+        this.files = files;
+        this.formData = new FormData();
+        this.payload = [];
+    }
+
+    public async prepare() {
+        for (let i = 0; i < this.files.length; i++) {
+            const metadata = await extractMetadata(this.files[i]);
+            const file = this.files[i];
+            const id = metadata.id;
+            this.payload.push({ id, metadata, file })
+        }
+    }
+
+    private buildFormData() {
+        Object.values(this.payload).forEach(({ metadata, file }) => {
+            this.formData.append(
+                "metadata",
+                new Blob([JSON.stringify(metadata)], { type: "application/html" })
+            )
+
+            this.formData.append(
+                `file-${metadata.id}`,
+                file,
+                file.name
+            )
+        })
+    }
+
+    public async send(): Promise<Response> {
+        this.buildFormData();
+        const url = "http://localhost:8081/api/upload"
+
+        //const url = `${this.baseURL}/${endpoint}`;
+        const options: RequestInit = {method: "POST", body: this.formData};
+
+        const response = await fetch(url, options);
+        await this.handleFailedUpload(response);
+        return await response.json();
+    }
+
+    private async handleFailedUpload(response: Response): Promise<void> {
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`Upload failed: ${response.status} ${response.statusText}`, errorText);
+            throw new Error(`Upload failed with status ${response.status}`);
+        }
+    }
+}
+
