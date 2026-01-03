4e2b23e6c10d85164d1ae639753cb8c205543ac2
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-01-03 00:37:22 +0000

    feat(backend): add upload service
    * add UploadSvc struct
    
    * add NewUploadService constructor
    
    * add Save method to parse multipart form and save metadata and file data
    
    * add saveFileData helper method to save file data in tmp filedir

---
+++ backend/src/core/files/service/upload.go
@@ -0,0 +1,89 @@
+package files
+
+import (
+	model "backend/src/core/files/model"
+	repository "backend/src/core/files/repository"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+)
+
+type UploadSvc struct {
+	repo *repository.FileRepository
+}
+
+func NewUploadService(fileRepo *repository.FileRepository) *UploadSvc {
+	return &UploadSvc{
+		repo: fileRepo,
+	}
+}
+
+func (svc *UploadSvc) Save(r *http.Request, ctx context.Context) error {
+	mr, err := r.MultipartReader()
+	if err != nil {
+		return err
+	}
+
+	var (
+		metadata model.MetaData
+		fileData model.FileData
+	)
+
+	for {
+		part, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		switch part.FormName() {
+		case "metadata":
+			if err := json.NewDecoder(part).Decode(&metadata); err != nil {
+				return err
+			}
+		case "file":
+			fileData = model.FileData{
+				Filename: part.FileName(),
+				Reader:   part,
+			}
+		}
+	}
+
+	if err := svc.repo.SaveMetaData(metadata, ctx); err != nil {
+		return err
+	}
+
+	if err := svc.saveFileData("backend/tempfiles", fileData); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc *UploadSvc) saveFileData(path string, data model.FileData) error {
+	err := os.MkdirAll(path, 0655)
+	if err != nil {
+		return err
+	}
+
+	tmp, err := os.CreateTemp(path, data.Filename+"-*")
+	if err != nil {
+		return err
+	}
+	defer func(tmp *os.File) {
+		err := tmp.Close()
+		if err != nil {
+			return
+		}
+	}(tmp)
+
+	if _, err := io.Copy(tmp, data.Reader); err != nil {
+		return err
+	}
+
+	return nil
+}
