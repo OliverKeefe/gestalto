a65e7472efc83f2a0188c69754e108c32f33a0c4
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-01-07 19:41:22 +0000

    feat(backend): add MetaDataDTO
    * add MetaDataDTO struct for mapping metadata request part to model

--- backend/src/core/files/service/upload.go
+++ backend/src/core/files/service/upload.go
@@ -14,6 +14,23 @@ type UploadSvc struct {
 	repo *repository.FileRepository
 }
 
+// MetaDataDTO is a Data Transfer Object for file Metadata.
+// When the frontend sends a multipart form, metadata is stored
+// as raw json,
+type MetaDataDTO struct {
+	Path             string `json:"path"`
+	RelativePath     string `json:"relativePath"`
+	LastModified     int64  `json:"lastModified"`
+	LastModifiedDate string `json:"lastModifiedDate"`
+	Size             uint64 `json:"size"`
+	FileType         string `json:"fileType"`
+
+	ID       string `json:"id"`
+	OwnerID  string `json:"ownerId"`
+	CheckSum string `json:"checkSum"`
+}
+
+// NewUploadService constructor for new UploadSvc (UploadService).
 func NewUploadService(fileRepo *repository.FileRepository) *UploadSvc {
 	return &UploadSvc{
 		repo: fileRepo,
