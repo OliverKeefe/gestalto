b7f0c8ed4d5b62fa56b7b2a901f2cdc853b54010
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-01-07 19:31:27 +0000

    refactor(backend): make SaveMetaData persist metadata
    query now persists all metadata mapped from service method

--- backend/src/core/files/repository/queries.go
+++ backend/src/core/files/repository/queries.go
@@ -4,6 +4,7 @@ import (
 	files "backend/src/core/files/model"
 	"backend/src/internal/metadb"
 	"context"
+	"log"
 )
 
 type FileRepository struct {
@@ -18,27 +19,37 @@ func NewFileRepository(db *metadb.MetadataDatabase) *FileRepository {
 
 func (repo *FileRepository) SaveMetaData(meta files.MetaData, ctx context.Context) error {
 	const query = `
-		INSERT INTO file_metadata (
-		                      file_name,
-		                      path,
-		                      size,
-		                      modified_at,
-		                      version,
-		                      owner
-		) VALUES ($1, $2, $3, $4, $5, $6)
-	`
+    	INSERT INTO file_metadata (
+    	    id,
+    	    file_name,
+    	    path,
+    	    size,
+    	    file_type,
+    	    modified_at,
+    	    uploaded_at,
+    	    version,
+    	    owner
+    	) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
+    `
+
+	log.Printf("metadata=%+v", meta)
 
 	var db = repo.db.Pool
-	_, err := db.Exec(
+	status, err := db.Exec(
 		ctx,
 		query,
+		meta.ID,
 		meta.FileName,
 		meta.Path,
 		meta.Size,
+		meta.FileType,
 		meta.ModifiedAt,
+		meta.CreatedAt,
 		meta.Version,
 		meta.Owner,
 	)
+
+	log.Printf("DB Status: %s", status)
 
 	return err
 }
