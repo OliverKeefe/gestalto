c820c13260f3340f7329d0589d1bbf6fb3fb248d
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-02-02 18:05:41 +0000

    feat(frontend): add object upload for IPFS
    * add uploadObject function
    * add makeDataBlobLike function

---
+++ frontend/src/app/features/shared/storacha/upload.ts
@@ -0,0 +1,45 @@
+import {ObjectType} from "@/app/features/shared/storacha/types.ts";
+import type {Metadata} from "@/app/features/shared/files/metadata.ts";
+import {StorachaClient} from "@/app/features/shared/storacha/client.ts";
+
+
+export async function uploadObject(
+    //spaceDID: string,
+    //delegation,
+    filedata: Uint8Array[],
+    metadata: Metadata[],
+    objectType: ObjectType
+): Promise<string | null> {
+
+
+    const account = await StorachaClient.login("test@test.galacy");
+    //const space = await StorachaClient.createSpace("test-space", {account})
+    await StorachaClient.setCurrentSpace(`did:key:${did}`);
+
+    switch (objectType) {
+        case ObjectType.FILE: {
+            const file = makeDataBlobLike(filedata[0], metadata[0]);
+            try {
+                const cid = await StorachaClient.uploadFile(file);
+                return cid.toString();
+            } catch (err) {
+                console.log("unable to upload to storacha network: ", err);
+                return null;
+            }
+        }
+
+        //case ObjectType.DIRECTORY: {}
+
+        //case ObjectType.CAR_FILE_SHARDS: {}
+    }
+    return null;
+}
+
+function makeDataBlobLike(filedata: Uint8Array, metadata: Metadata): File {
+    try {
+        return new File([filedata], metadata.path);
+    } catch (err) {
+        console.log("unable to make file blob like: ", err)
+        return new File(0, null); // fix this abomination.
+    }
+}
