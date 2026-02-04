5df8915dc24be4a282d84f0b5c99a6049937e990
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-02-04 23:51:21 +0000

    refactor(frontend): improve app config
    * remove dead code
    * add new wiring

--- backend/src/internal/app/app.go
+++ backend/src/internal/app/app.go
@@ -5,15 +5,10 @@ import (
 	"backend/src/internal/auth"
 	"backend/src/internal/db/metadb"
 	"backend/src/internal/middleware"
-	midware "backend/src/internal/middleware"
-	fileshandler "backend/src/usecase/files/handler"
-	filesrepo "backend/src/usecase/files/repository"
-	filesvc "backend/src/usecase/files/service"
 	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 func Run() error {
@@ -32,9 +27,10 @@ func Run() error {
 		return err
 	}
 
-	//TODO: Chain this instead, probably following functional options.
-	handler := middleware.EnableCORS(registerRoutes(a, db))
-
+	appMux := http.NewServeMux()
+	var handler http.Handler = appMux
+	handler = middleware.EnableCORS(handler)
+	router.RegisterFileRoutes(appMux, a, db)
 	srv := &http.Server{
 		Addr:    ":8081",
 		Handler: handler,
@@ -44,87 +40,10 @@ func Run() error {
 	return srv.ListenAndServe()
 }
 
-func registerRoutes(a *auth.Authenticator, metadataDB *metadb.MetadataDatabase) *http.ServeMux {
-	filesRepo := filesrepo.NewRepository(metadataDB)
-	filesService := filesvc.NewService(filesRepo)
-	filesHandler := fileshandler.NewHandler(filesService)
-
-	return router.New(
-		router.Handle(
-			"POST /api/files/upload",
-			midware.Protect(a, http.HandlerFunc(filesHandler.Upload)),
-		),
-		router.Handle("POST /api/files/getall",
-			http.HandlerFunc(filesHandler.GetAll),
-		),
-		router.Handle("POST /api/files/get",
-			http.HandlerFunc(filesHandler.FindMetadata),
-		),
-	)
-}
-
 func registerKeycloakAuth(issuer string, jwksURL string) (*auth.Authenticator, error) {
 	authenticator, err := auth.New(issuer, jwksURL)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create new authenticator, %v", err)
 	}
 	return authenticator, nil
-}
-
-type Config struct {
-	BaseURL          string
-	HTTPClient       *http.Client
-	MetadataDatabase *metadb.MetadataDatabase
-	HTTPServer       *http.Server
-	Multiplexer      *http.ServeMux
-}
-
-type Option func(*Config)
-
-func NewConfig(opts ...Option) Config {
-	cfg := Config{
-		BaseURL: "",
-		HTTPClient: &http.Client{
-			Timeout: 10 * time.Second,
-		},
-		MetadataDatabase: &metadb.MetadataDatabase{
-			Pool: nil,
-		},
-		HTTPServer:  nil,
-		Multiplexer: nil,
-	}
-	for _, opt := range opts {
-		opt(&cfg)
-	}
-	return cfg
-}
-
-func WithBaseURL(baseUrl string) Option {
-	return func(c *Config) {
-		c.BaseURL = baseUrl
-	}
-}
-
-func WithHTTPClient(client *http.Client) Option {
-	return func(c *Config) {
-		c.HTTPClient = client
-	}
-}
-
-func WithMetadataDB(mdb *metadb.MetadataDatabase) Option {
-	return func(c *Config) {
-		c.MetadataDatabase.Pool = mdb.Pool
-	}
-}
-
-func WithHTTPServer(srv *http.Server) Option {
-	return func(c *Config) {
-		c.HTTPServer = srv
-	}
-}
-
-func WithMux(mux *http.ServeMux) Option {
-	return func(c *Config) {
-		c.Multiplexer = mux
-	}
 }
